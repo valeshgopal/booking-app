8761a46a02c0a569ca56eaa8b70ca889181dddb3
Valesh Gopal <valesh.dev@gmail.com>
2022-11-04 22:55:01 +0530

    update app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route eaxct path='/' element={<Home />} />
         <Route path='/hotels' element={<List />} />
         <Route path='/hotels/:id' element={<Hotel />} />
       </Routes>
